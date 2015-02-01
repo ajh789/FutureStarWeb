39da8c5f52a8b31f4db3c4b185a810e1fe597628
Andy Jiang H <andy.h.jiang@gmail.com>
2015-02-02 00:27:31 +0800

    Copy "manage.jsp" to "manage_school.jsp", and "manage.js" to "manage_school.js".

--- WebContent/js/manage_school.js
+++ WebContent/js/manage_school.js
@@ -1,13 +1,300 @@
-﻿function on_button_create()
+document.getElementByIdx_x = function(id) {
+	if (typeof id == 'string') {
+		return document.getElementById(id);
+	} else {
+		throw new error('Please pass a string as an ID!');
+	}
+};
+
+var g_manageschool_url = "/futurestar/manageschool.do?reqfrom=wap";
+var g_manageschool_select_url = g_manageschool_url + "&action=select";
+var g_manageschool_insert_url = g_manageschool_url + "&action=insert";
+var g_manageschool_update_url = g_manageschool_url + "&action=update";
+var g_manageschool_delete_url = g_manageschool_url + "&action=delete";
+
+var g_schools = null; // Array of schools.
+var g_schools_head = null; // Current head.
+var g_schools_tail = null; // Current tail.
+var g_privilege = 0;
+
+function reqData()
 {
-  document.getElementById("span_content").innerHTML = "<b>新建学校</b><br/>" +
-    "<form action='do_create_school' method='post'>" + 
-    "学校名称：<input type='text' name='school_name'>" +
-    "<input type='submit' value='创建'>" +
-    "</form>";
+	var url = g_manageschool_select_url;
+
+//	if (g_schools_tail != null) {
+//		url += "&baseid=" + g_schools_tail;
+//	}
+
+	var schoolname = $("#text_schoolname").val();
+	if (schoolname != "") {
+		url += "&name=" + schoolname;
+	}
+
+	$.get(url, handleSelectResponse);
 }
 
-function on_button_search()
+function reqDataUp()
 {
-  document.getElementById("span_content").innerHTML="<b>My First JavaScript Function</b>";
+	var url = g_manageschool_select_url;
+
+	if (g_schools_head != null) {
+		url += "&baseid=" + g_schools_head + "&goes=up";
+	}
+
+	var schoolname = $("#text_schoolname").val();
+	if (schoolname != "") {
+		url += "&name=" + schoolname;
+	}
+
+	$.get(url, handleSelectResponse);
+}
+
+function reqDataDown()
+{
+	var url = g_manageschool_select_url;
+
+	if (g_schools_tail != null) {
+		url += "&baseid=" + g_schools_tail;
+	}
+
+	var schoolname = $("#text_schoolname").val();
+	if (schoolname != "") {
+		url += "&name=" + schoolname;
+	}
+
+	$.get(url, handleSelectResponse);
+}
+
+function reqDataFromTo()
+{
+	var url = g_manageschool_select_url + "&mode=1";
+
+	if (g_schools_head != null)
+		url += "&fromid=" + g_schools_head;
+
+	if (g_schools_tail != null)
+		url += "&toid=" + g_schools_tail;
+
+	$.get(url, handleSelectResponse);
+}
+
+function handleSelectResponse(data, status) {
+	var tmp = "";
+	var debug = "";
+
+	if (status == "success") { // 200 OK
+		var ret = null;
+		if (typeof data == "object") { // object
+			ret = data;
+		} else { // string
+			ret = eval("("+data+")"); // Transit JSON string to JSON object.
+		}
+
+		// Five members: retcode, retinfo, actionx, schools, prvlege
+		if (ret.retcode == 0) { // OK
+			g_privilege = ret.prvlege;
+			var schools = ret.schools; // Array of schools.
+			if (schools.length > 0) {
+				g_schools = new Array(); // Allocate a new array.
+				g_schools = g_schools.concat(schools);
+				g_schools_head = schools[0].CREATION;
+				g_schools_tail = schools[schools.length-1].CREATION;
+				tmp += generateTableOfSchools();
+				setSpanContentInnerHTML(tmp);
+//				hideButtonReqData();
+				showButtonReqDataUp();
+				showButtonReqDataDown();
+			} else {
+				debug += "没有更多数据可加载！";
+			}
+		} else {
+			g_privilege = 0;
+			debug += ret.retinfo;
+		}
+	} else {
+		debug += data;
+	}
+
+	if (debug != "") {
+		showSpanDebugMsg(); // Show
+		setSpanDebugMsgInnerHTML(debug);
+	} else {
+		hideSpanDebugMsg(); // Hide
+	}
+}
+
+function handleUpdateResponse(data, status) {
+	if (status == "success") {
+		var ret = null;
+		if (typeof data == "object") { // object
+			ret = data;
+		} else { // string
+			ret = eval("("+data+")"); // Transit JSON string to JSON object.
+		}
+//		window.alert(ret.retcode + ": " + ret.retinfo);
+		if (ret.retcode == 0) {
+			// Trigger a query
+			reqDataFromTo();
+		}
+	}
+}
+
+function generateTableOfSchools() {
+	var html = "";
+
+	if (g_schools.length > 0) {
+		html += "<table id='schools' border='1'>";
+		html += "<tr>";
+		html += "<th>&nbsp;</th>";       // Column 1
+		html += "<th>学校</th>"; // Column 2
+		html += "<th>操作</th>";     // Column 3
+		html += "</tr>";
+	}
+
+	for (var i=0; i<g_schools.length; i++) {
+		html += "<tr>"; // Row stars.
+		// Column 1
+		if (g_schools[i].LOGO != "" && g_schools[i].LOGO != "null") {
+			html += "<td><img src='" + g_schools[i].LOGO +"' alt='logo' class='img_school_logo' /></td>";
+		} else {
+			html += "<td>" + g_schools[i].LOGO + "</td>";
+		}
+		// Column 2
+		html += "<td id='school_details'><b>" + g_schools[i].NAME + "</b>(" + g_schools[i].ID + ")<br/>" + g_schools[i].CREATION +"<br/>" + g_schools[i].INTRO + "</td>";
+		// Column 3
+		html += "<td>";
+		if (g_privilege & 0x4)
+			html += "<input type='button' value='删除' onclick='onButtonDeleteSchool()' />";
+		if (g_privilege & 0x2)
+			html += "<input type='button' value='修改' onclick='onButtonEditSchool(\"" + g_schools[i].ID +"\")' />";
+		html += "</td>";
+		html += "</tr>"; // Row ends.
+	}
+
+	if (g_schools.length > 0) {
+		html += "</table>";
+	}
+
+	return html;
+}
+
+function hideButtonReqData() {
+	$("#button_reqdata").hide();
+}
+
+function showButtonReqDataUp() {
+	$("#button_reqdata_up").show();
+}
+
+function showButtonReqDataDown() {
+	$("#button_reqdata_down").show();
+}
+
+function hideSpanDebugMsg() {
+	$("#span_debugmsg").hide();
+}
+
+function showSpanDebugMsg() {
+	$("#span_debugmsg").show();
+}
+
+function setSpanDebugMsgInnerHTML(innerHTML) {
+	$("#span_debugmsg").html(innerHTML);
+}
+
+function setSpanContentInnerHTML(innerHTML) {
+	$("#span_content").html(innerHTML);
+}
+
+function onButtonEditSchool(id) {
+	if (typeof id == 'string') {
+		var found = false;
+		var school = null;
+		for (var i=0; i<g_schools.length; i++) {
+			if (g_schools[i].ID == id) {
+				found = true;
+				school = g_schools[i];
+				break;
+			}
+		}
+		if (found) {
+			hideSpanDebugMsg();
+			generateEditSchoolHtml(school);
+		}
+	} else {
+		throw new error('Please pass a string as an ID!');
+	}
+}
+
+function onButtonDeleteSchool()
+{
+	window.alert("暂不支持删除操作！");
+}
+
+function generateEditSchoolHtml(school)
+{
+	var html = "";
+	html += "<table class='table_school_edit'>";
+	html += "  <tr>";
+	html += "    <td>学校徽标：</td>";
+	html += "    <td><img id='school_edit_logo' src='' alt='logo' class='img_school_logo' /></td>";
+	html += "  </tr>";
+	html += "  <tr>";
+	html += "    <td>学校名称：</td>";
+	html += "    <td><span id='school_edit_name'></span></td>";
+	html += "  </tr>";
+	html += "  <tr>";
+	html += "    <td>注册时间：</td>";
+	html += "    <td><span id='school_edit_creation'></span></td>";
+	html += "  </tr>";
+	html += "  <tr>";
+	html += "    <td>更新时间：</td>";
+	html += "    <td><span id='school_edit_lastupdate'></span></td>";
+	html += "  </tr>";
+	html += "  <tr>";
+	html += "    <td>当前状态：</td>";
+	html += "    <td>";
+	html += "      <input id='school_edit_islocked_true'  type='radio' name='islocked' value='true' />锁定";
+	html += "      <input id='school_edit_islocked_false' type='radio' name='islocked' value='false' />未锁定";
+	html += "    </td>";
+	html += "  </tr>";
+	html += "  <tr>";
+	html += "    <td>学校介绍：</td>";
+	html += "    <td><textarea id='school_edit_intro' rows='10' cols='80'></textarea></td>";
+	html += "  </tr>";
+	html += "  <tr>";
+	html += "    <td>&nbsp;&nbsp;</td>";
+	html += "    <td>";
+	html += "      <input type='hidden' id='school_edit_id' name='id' value='' />";
+	html += "      <input type='button' value='更新' onclick='onButtonCommitEditSchool()' />";
+	html += "      <input type='button' value='取消' onclick='onButtonCancelEditSchool()' />";
+	html += "    </td>";
+	html += "  </tr>";
+	html += "</table>";
+	setSpanContentInnerHTML(html);
+	$("#school_edit_id").val(school.ID);
+	$("#school_edit_logo").attr("src", school.LOGO);
+	$("#school_edit_name").html(school.NAME);
+	$("#school_edit_creation").html(school.CREATION);
+	$("#school_edit_lastupdate").html(school.LASTUPDATE);
+	$("#school_edit_islocked_true").attr("checked", (school.ISLOCKED)?"checked":"");
+	$("#school_edit_islocked_false").attr("checked", (!school.ISLOCKED)?"checked":"");
+	$("#school_edit_intro").val(htmlDecode(school.INTRO)); // Remember to do html decode.
+}
+
+function onButtonCommitEditSchool() {
+	var id = $("#school_edit_id").val();
+	var name = $("#school_edit_name").html();
+	var intro = $("#school_edit_intro").val();
+	intro = htmlEncode(intro); // Remember to do html encode.
+	$.post(
+		g_manageschool_update_url, 
+		{"id":id, "name":name, "intro":intro}, 
+		handleUpdateResponse
+	);
+}
+
+function onButtonCancelEditSchool() {
+	var tmp = generateTableOfSchools();
+	setSpanContentInnerHTML(tmp);
 }
--- WebContent/manage_school.jsp
+++ WebContent/manage_school.jsp
@@ -5,45 +5,64 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>学校管理</title>
 <style type="text/css">
-div#container {width:100%}
-div#header    {background-color:#99bbbb;}
-div#menu      {background-color:#ffff99; height:600px; width:15%; float:left; overflow:auto;}
-div#content   {background-color:#EEEEEE; height:600px; width:85%; float:left; overflow:auto;}
-div#footer    {background-color:#99bbbb; clear:both; text-align:center;}
-h1 {margin-bottom:0;}
-h2 {margin-bottom:0; font-size:14px;}
-ul {margin:0;}
-li {list-style:none;}
+@import "js/jquery/ui/1.11.2/themes/smoothness/jquery-ui.css";
+@import "css/manage.css"
 </style>
+<script type="text/javascript" src="js/jquery/jquery-1.10.2.min.js" charset="UTF-8"></script>
+<script type="text/javascript" src="js/jquery/ui/1.11.2/jquery-ui.min.js" charset="UTF-8"></script>
+<script type="text/javascript" src="js/common.js" charset="UTF-8"></script>
 <script type="text/javascript" src="js/manage_school.js" charset="UTF-8"></script>
+<script type="text/javascript">
+$(function() {
+  $( "#content_tab" ).tabs();
+});
+</script>
 </head>
 <body>
 <%
 if (session.getAttribute("name") == null) {
-  response.sendRedirect("/futurestar/login.jsp");
-} else {
+	session.setAttribute("frompage", "/futurestar/manage_school.jsp");
+ 	response.sendRedirect("/futurestar/login.jsp");
+}
 %>
 <div id="container">
-	<div id="header">
-		<h1>Main Title of Web Page</h1>
-	</div>
-	<div id="menu">
-		<h2>菜单</h2>
-		<ul>
-		<li><a href="manage_school.jsp">学校管理</a></li>
-		<li><a href="">教师管理</a></li>
-		<li><a href="">学生管理</a></li>
-		</ul>
-	</div>
-	<div id="content">
-		<input id="button_create" type="button" value="新建" onclick="on_button_create()" />
-		<input id="button_search" type="button" value="查找" onclick="on_button_search()" /><br/>
-		<span id="span_content"></span>
-	</div>
-	<div id="footer">Copyright FutureStar.com.cn</div>
+<div id="header">
+<h1>Main Title of Web Page</h1>
 </div>
-<%
-} // else
-%>
+<div id="menu">
+<h2>菜单</h2>
+<ul>
+<li><a href="manage_school.jsp">学校管理</a></li>
+<li><a href="manage_teacher.jsp">教师管理</a></li>
+<li><a href="manage_parent.jsp">家长管理</a></li>
+<li><a href="manage_student.jsp">学生管理</a></li>
+</ul>
+</div>
+<div id="content">
+	<div id="content_tab">
+		<ul>
+			<li><a href="#content_tab_search">查找</a></li>
+			<li><a href="#content_tab_addnew">新增</a></li>
+		</ul>
+		<div id="content_tab_search">
+			<div id="content_tab_search_upper">
+			学校名称：<input id="text_schoolname" type="text" value="南京" />
+			<input id="button_reqdata" type="button" value="查找" onclick="reqData()" />
+			<input id="button_reqdata_up" type="button" value="上一页" onclick="reqDataUp()" />
+			<input id="button_reqdata_down" type="button" value="下一页" onclick="reqDataDown()" />
+			<span id="span_debugmsg"></span>
+			</div> <!-- content_tab_search_upper -->
+			<br/>
+			<div id="content_tab_search_lower">
+			<span id="span_content"></span>
+			</div> <!-- content_tab_search_lower -->
+		</div> <!-- content_tab_search -->
+		<div id="content_tab_addnew">
+		</div> <!-- content_tab_addnew -->
+	</div> <!-- content_tab -->
+</div> <!-- content -->
+<div id="footer">Copyright FutureStar.com.cn</div>
+</div> <!-- container -->
+
 </body>
 </html>
